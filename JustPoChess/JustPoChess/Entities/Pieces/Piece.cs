857a5e9820817281cbd28709b10de9f0be5c0cd0
Xadera <ragnarokha@gmail.com>
2017-07-19 18:53:03 +0300

    Initial Structure Commit

---
+++ JustPoChess/JustPoChess/Entities/Pieces/Piece.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JustPoChess.Interfaces;
+
+namespace JustPoChess.Entities
+{
+    public class Piece : IMovable, IDrawable
+    {
+        public virtual void Draw()
+        {
+            
+        }
+
+        public virtual void Move()
+        {
+            
+        }
+    }
+}
---
+++ JustPoChess/JustPoChess/Entities/Pieces/Queen/WhiteQueen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustPoChess.Entities.Pieces.Queen
+{
+    public class WhiteQueen : Queen
+    {
+    }
+}

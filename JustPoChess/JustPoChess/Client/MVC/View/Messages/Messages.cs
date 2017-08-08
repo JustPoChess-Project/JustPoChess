331f5dc0c8e8dbb226927bf219599dedb6cecbd6
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-08-08 15:38:02 +0300

    Added needed messages

--- JustPoChess/JustPoChess/Client/MVC/View/Messages/Messages.cs
+++ JustPoChess/JustPoChess/Client/MVC/View/Messages/Messages.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JustPoChess.Client.MVC.View.Messages
+﻿namespace JustPoChess.Client.MVC.View.Messages
 {
     public static class Messages
     {
         public const string FontWarrning = "Please change console font to MS Gothic with size 72!";
         public const string FontColorWarrning = "For best experience use dark gray font color!";
+        public const string CheckMateMessage = "Checkmate";
+        public const string PawnPromotionMessage = "Choose between Queen, Bishop, Knight or Rook.";
+        public const string CheckedMessage = "Check!";
     }
 }
